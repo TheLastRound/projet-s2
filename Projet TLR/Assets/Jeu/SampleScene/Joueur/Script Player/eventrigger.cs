3581734a529169a32bab45f8f185552ac8001067
kemil.bina <kemil.bina@epita.fr>
2022-03-27 00:27:56 +0100

    oue on va recup le jeu de khatir

---
+++ Projet TLR/Assets/Jeu/SampleScene/Joueur/Script Player/eventrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CutsceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    void OnTriggerEnter(Collider other)
+    {
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+    }
+}
