d3a740335ed04e91b1dbe38591924f6b4cc2e715
kemil.bina <kemil.bina@epita.fr>
2022-04-25 13:20:28 +0200

    Sans la Maison

---
+++ Projet TLR/Assets/Gameplay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Gameplay : MonoBehaviour
+{
+    private int nBItem;
+
+    private GameObject Feuille;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
